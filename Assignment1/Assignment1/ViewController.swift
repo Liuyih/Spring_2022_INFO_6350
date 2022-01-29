dc9a7a91b74b1a05391addd495f6e02b99c2e404
Liuyih <851563210@qq.com>
2022-01-28 22:20:20 -0800

    firstName lastName check done

--- Assignment1/Assignment1/ViewController.swift
+++ Assignment1/Assignment1/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtFirstName: UITextField!
+    @IBOutlet weak var txtLastName: UITextField!
+    @IBOutlet weak var lblHello: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func sayHellofUNCTION(_ sender: Any) {
+        let firstName = txtFirstName.text!
+        
+        let lastName = txtLastName.text!
+        
+        if firstName.isEmpty && lastName.isEmpty {
+            lblHello.text = "Plz enter fist and last name"
+        }
+        
+        else if !firstName.isEmpty && lastName.isEmpty {
+            lblHello.text = "Enter Last Name"
+        }
+        
+        else if !lastName.isEmpty && firstName.isEmpty {
+            lblHello.text = "Enter First Name"
+        }
+        
+        else if !firstName.isEmpty && !lastName.isEmpty {
+            lblHello.text = "Hello \(firstName) \(lastName)"
+        }
+    }
+    
 }
 
